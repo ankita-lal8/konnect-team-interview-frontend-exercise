ad6215f8fef2ceb6fd54ca5ad7ffe2c9812b5b0f
alal <ankital@zeta.tech>
2024-11-30 21:24:48 +0530

    Added Page home + added services in composables + deleted service catalog + added assets folder

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
 <template>
   <header>
     <div class="nav">
-      <router-link to="/">
-        Home
-      </router-link>
+      <GlobalHeader />
     </div>
   </header>
   <main>
@@ -13,42 +11,14 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import GlobalHeader from '@/components/GlobalHeader/index.vue'
 
 export default defineComponent({
   name: 'App',
+  components: {
+    GlobalHeader,
+  },
 })
 </script>
 
-<style lang="scss" scoped>
-.nav {
-  border-bottom: 1px solid #333;
-  margin-bottom: 4rem;
-  padding: 16px;
-
-  a {
-    color: blue;
-    font-weight: 700;
-
-    &.router-link-exact-active {
-      color: green;
-    }
-  }
-}
-</style>
-
-<style lang="scss">
-// Unscoped styles for html and body
-html {
-  font-size: 10px;
-  margin: 0;
-  padding: 0;
-}
-
-body {
-  font-family: Arial, sans-serif;
-  font-size: 1.6rem;
-  font-weight: 400;
-  line-height: 1.2;
-  margin: 0 auto;
-}
-</style>
+<style lang="scss" scoped></style>
