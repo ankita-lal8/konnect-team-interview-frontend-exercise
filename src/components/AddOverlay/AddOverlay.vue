1baee18699bbc3bcc5f81db0eed8e44fc60d2a1d
alal <ankital@zeta.tech>
2024-11-30 21:27:33 +0530

    Handled Loading and Error states with AddService modal

---
+++ src/components/AddOverlay/AddOverlay.vue
@@ -0,0 +1,104 @@
+<template>
+  <div class="overlay">
+    <div class="overlay--modal">
+      <div class="overlay--modal-header">
+        <h2>Add Service Package</h2>
+        <span
+          class="close-button cursor-pointer"
+          @click="emit('close_add_overlay')"
+        >X</span>
+      </div>
+      <p>What kind of item would you like to add?</p>
+      <div class="button-group">
+        <button
+          class="cancel-button cursor-pointer"
+          type="button"
+          @click="emit('close_add_overlay')"
+        >
+          Cancel
+        </button>
+        <button
+          class="add-button cursor-pointer"
+          type="submit"
+          @click="emit('close_add_overlay')"
+        >
+          Add item
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+const emit = defineEmits(['close_add_overlay'])
+</script>
+<style lang="scss" scoped>
+.overlay {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  background-color: var(--overlay-bg-color);
+  &--modal {
+    background-color: var(--white);
+    border-radius: 20px;
+    max-width: 490px;
+    width: 100%;
+    padding: 50px;
+    box-shadow: 0px 10px 12px var(--modal-shadow-color);
+    position: relative;
+    &-header {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      margin-bottom: 20px;
+    }
+    &-header h2 {
+      font-size: 2rem;
+      font-weight: 700;
+    }
+    .close-button {
+      position: relative;
+      top: 0;
+      font-size: 24px;
+      cursor: pointer;
+    }
+    .button-group {
+      display: flex;
+      justify-content: space-around;
+      margin: 30px;
+    }
+
+    .cancel-button,
+    .add-button {
+      font-weight: 500;
+      font-size: 1rem;
+      background: none;
+      border: none;
+      border-radius: 3px;
+      cursor: pointer;
+      padding: 12px 30px;
+    }
+
+    .cancel-button {
+      color: var(--secondary-cta-color);
+    }
+
+    .add-button {
+      background-color: var(--primary-cta);
+      color: var(--white);
+    }
+
+    /* Button Hover Styles */
+    .cancel-button:hover {
+      color: var(--white);
+      background-color: var(--secondary-cta-hover-color);
+    }
+
+    .add-button:hover {
+      background-color: var(--primary-cta-hover);
+    }
+  }
+}
+</style>
---
+++ src/components/DataLoader.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="loader" />
+</template>
+<script setup lang="ts"></script>
+<style lang="scss" scoped>
+.loader {
+  border: 3px solid var(--loader-border-color);
+  border-radius: 50%;
+  border-top: 3px solid var(--loader-border-spinning-color);
+  width: 30px;
+  height: 30px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  -webkit-animation: spin 2s linear infinite; /* Safari */
+  animation: spin 2s linear infinite;
+}
+/* Safari */
+@-webkit-keyframes spin {
+  0% {
+    -webkit-transform: rotate(0deg);
+  }
+  100% {
+    -webkit-transform: rotate(360deg);
+  }
+}
+
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
